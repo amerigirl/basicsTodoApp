537987a508121b8e4fa5ba81750673adb1c9ffac
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-10-23 23:11:46 -0700

    practice component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Header from './Components/Header'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+      <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
