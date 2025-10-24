6cedd1a92bed96f019187e9b2aa81806a8b5447e
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-10-24 09:39:52 -0700

    added data file and todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Header from "./Components/Header/Header";
-
+import ToDo from "./Components/ToDo/ToDo";
 
 /** Create a todo app and instead of using a backend use settimeout to create async functionality
  * Create a todos list in a separate folder
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <ToDo/>
     </>
   );
 }
---
+++ src/Data/ToDoList.tsx
@@ -0,0 +1,39 @@
+const toDoList = [
+  {
+    id: 1,
+    task: "Get food",
+    completed: false,
+  },
+  {
+    id: 2,
+    task: "Study React",
+    completed: false,
+  },
+  {
+    id: 3,
+    task: "School work",
+    completed: false,
+  },
+  {
+    id: 4,
+    task: "Clean the bathroom",
+    completed: false,
+  },
+  {
+    id: 5,
+    task: "Vaccum",
+    completed: false,
+  },
+  {
+    id: 6,
+    task: "Laundry",
+    completed: false,
+  },
+  {
+    id: 7,
+    task: "Find quiet time",
+    completed: false,
+  },
+];
+
+export default toDoList;
